c6779752769d82b0b0c6d0333950bfd0c69570d2
JayaPradhi <pradeepvlytr29@gmail.com>
2024-03-10 21:31:21 +0530

    load

--- eda.py
+++ eda.py
@@ -1,2 +1,11 @@
-#data load
+#LOAD A TITANIC DATA SET
 df=pd.read_csv("D:\\GIT\\titanic.csv")
+
+#BASIC ANALSYS:
+print(df.head(5))
+print(df.info())
+print(df.columns)
+print(len(df))
+print(df.describe)
+print(df.isnull().sum())
+
