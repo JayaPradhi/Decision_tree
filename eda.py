c8ab0af41e3efb7f405278bb35625f1ca559f1b4
JayaPradhi <pradeepvlytr29@gmail.com>
2024-03-10 22:20:01 +0530

    pie

--- eda.py
+++ eda.py
@@ -9,3 +9,14 @@ print(len(df))
 print(df.describe)
 print(df.isnull().sum())
 
+#PIE CHART FOR SURVIVIED COLUMN
+f,ax=plt.subplots(1,2,figsize=(10,8))
+explode=[0.1] * len(df['Survived'].value_counts())
+df['Survived'].value_counts().plot.pie(ax=ax[0],explode=explode,autopct='%1.1f%%')
+ax[0].set_title('pie chart')
+sns.countplot(x="Survived",data=df,ax=ax[1])
+ax[1].set_title("Count plot")
+plt.show()
+
+
+
