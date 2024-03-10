244e102a25b4cf4e80e4c6ab06388929887539dd
PRADEEP J <127920413+JayaPradhi@users.noreply.github.com>
2024-03-10 21:23:06 +0530

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier #Decision Tree
+from sklearn.model_selection import train_test_split #training and testing data split
+from sklearn import metrics #accuracy measure
+from sklearn.metrics import confusion_matrix
