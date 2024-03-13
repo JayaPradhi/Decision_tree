04d0c30c60f03f2041a3d301941ad6ac80195ee4
PRADEEP J <127920413+JayaPradhi@users.noreply.github.com>
2024-03-13 22:06:55 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
+train,test=train_test_split(data,test_size=0.3,random_state=0,stratify=data['Survived'])
+train_X=train[train.columns[1:]]
+train_Y=train[train.columns[:1]]
+test_X=test[test.columns[1:]]
+test_Y=test[test.columns[:1]]
+X=data[data.columns[1:]]
+Y=data['Survived']
+
+#MODEL
 model=DecisionTreeClassifier()
 model.fit(train_X,train_Y)
 prediction4=model.predict(test_X)
